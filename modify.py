f23033562ca82063be05a67a4b2576eaa6952440
Roshaun <roshauninfantr@outlook.com>
2022-03-05 13:50:00 +0530

    module deleted

--- modify.py
+++ modify.py
@@ -1,14 +1,19 @@
 import mysql.connector as sqltor
+import mysql.connector.errors
 thecon = sqltor.connect(host='localhost', user='root', passwd='1234', database='vnps')
 thecur = thecon.cursor()
 cur = thecon.cursor()
 
 
 def update():
-    person = input('Enter name of alumni: ')
-    column = input('Enter the column to updated: ')
-    new = input('Enter new data to be inserted: ')
-    stmt = f"UPDATE alumni1 SET {column}='{new}' WHERE Name='{person}'"
-    thecur.execute(stmt)
-    thecon.commit()
-    print('Data Updated Successfully\n')
+    try:
+        person = input('Enter name of alumni: ')
+        column = input('Enter the column to updated: ')
+        new = input('Enter new data to be inserted: ')
+        stmt = f"UPDATE alumni1 SET {column}='{new}' WHERE Name='{person}'"
+        thecur.execute(stmt)
+        thecon.commit()
+        print('Data Updated Successfully\n')
+    except mysql.connector.errors.DataError:
+        print("Invalid input!! Please try again \n\n")
+        update()
